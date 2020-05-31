0b2b5d559e383c27113dc4f5074b37ca3fdec70f
Shiva Narrthine <shivanarrthine@gmail.com>
2020-05-31 20:45:35 +0800

    built all pages, responsive fixes, theme switcher

---
+++ public/javascripts/main.js
@@ -0,0 +1,88 @@
+
+var currentTheme;
+var lightThemePath = "/public/css/light.css";
+var darkThemePath = "/public/css/dark.css";
+
+
+$(document).ready(function(){
+
+    // detect if there is an existing theme selection
+    currentTheme = localStorage.getItem("currentTheme");
+
+    console.log("Current theme is " +currentTheme);
+
+    if(currentTheme === "light"){
+        // change toggle to dark and change theme
+        $("#themeSwitcher").attr("checked", true)
+        switchToLightTheme()
+    }
+
+    // detect toggle interaction
+    $("#themeSwitcher").on('change', toggleTheme)
+
+});
+
+// Theme switching
+function toggleTheme(){
+    // detect if there is an existing session, pull currrent theme
+    currentTheme = localStorage.getItem("currentTheme");
+    if(!currentTheme){
+        // default to dark
+        currentTheme = "dark";
+        console.log("defaulted to dark")
+    }
+    
+    if(currentTheme === "light"){
+        switchToDarkTheme()
+    } else if (currentTheme === "dark"){
+        switchToLightTheme()
+    }
+}
+
+function switchToDarkTheme(){
+    loadThemeFile(lightThemePath, darkThemePath);
+    saveCurrentTheme("dark");
+
+    console.log("switched to dark theme")
+}
+
+function switchToLightTheme(){
+    loadThemeFile(darkThemePath, lightThemePath);
+    saveCurrentTheme("light");
+    console.log("Switced to light theme")
+}
+
+function loadThemeFile(oldPath, newPath){
+    $("link[href='" + oldPath + "']").attr('href',newPath);
+}
+
+function saveCurrentTheme(theme){
+    localStorage.setItem("currentTheme", theme);
+}
+
+// email copy functionality
+function copyEmail(){
+    // get email from hidden input
+    var input = document.getElementById("email")
+    var value = input.select();
+    
+    // copy to clipboard
+    document.execCommand('copy');
+
+    // show copied message
+    $("#copyEmailBtn").text("Email copied")
+    $("#copyEmailBtn").addClass('active')
+    setTimeout(function(){
+        $("#copyEmailBtn").text("Copy email")
+        $("#copyEmailBtn").removeClass('active')
+    }, 2000)
+}
+
+// smooth scroll fallback
+$(document).on('click', 'a[href^="#"]', function (event) {
+    event.preventDefault();
+
+    $('html, body').animate({
+        scrollTop: $($.attr(this, 'href')).offset().top
+    }, 500);
+});
